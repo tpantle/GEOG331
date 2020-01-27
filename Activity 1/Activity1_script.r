52d1473e3bbf681655a2d1c20165e1241d6ac675
Tyler Pantle '20 <tpantle@colgate.edu>
2020-01-27 11:01:28 -0500

    script submission for activity 1

---
+++ Activity 1/Activity1_script.r
@@ -0,0 +1,31 @@
+# Activity 1
+print("hello R world")
+
+# three times three
+3*3
+# ten plus two 
+10+2
+# name my number
+a <- 2446
+# multiply my number by 5
+5*a
+# divide my number by 2
+a/2
+# make a vector of numbers using "c(...,..,...)"
+b <- c(2446,40,10000)
+# divide all numbers by 2
+b/2
+# multiply variables
+b*a
+
+# Question 3:
+# Run 3 calculations, at least one with a vector with 4 numbers and a variable name
+
+# ten times twelve
+10*12
+# 20 minus 3
+20-3
+# make a vector
+x <- c(172,86,96)
+# divide all vector numbers by 2
+x/2
